4a13b11477997e8d559fcd6eeece8278860e6e4e
LeoBang <bluke8489@gmail.com>
2021-12-11 14:59:10 +0900

    feat: add jwt verify middleware

---
+++ src/middlewares/index.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const baseResponse = require('../../config/baseResponseStatus');
+const { response, errResponse} = require('../../config/response');
+
+require('dotenv').config();
+
+exports.verifyToken = async (req, res, next) => {
+    const token = req.headers['x-access-token'] || req.query.token;
+    
+    // jwt가 없을 경우 
+    if (!token)
+        return res.send(errResponse(baseResponse.TOKEN_EMPTY))
+
+    try {
+        req.decoded = await jwt.verify(token, process.env.JWT_SECRET);
+        return next();
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            return res.send(errResponse(baseResponse.TOKEN_EXPIRED));
+        };
+
+        return res.send(errResponse(baseResponse.TOKEN_VERIFICATION_FAILURE));
+    }
+    
+}
