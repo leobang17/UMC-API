2f1803f1030d1d68375616ce222f45e3fbf245e1
LeoBang <bluke8489@gmail.com>
2021-12-14 17:24:52 +0900

    add express-session

--- config/express.js
+++ config/express.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const passportTest = require('../src/passport');
 
 
 // Require Router
@@ -22,9 +23,20 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKEI_SECRET));
+app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors());
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false
+    },
+    name: 'session-cookie'
+}))
+app.use(passportTest);
 
 
 // Routers
